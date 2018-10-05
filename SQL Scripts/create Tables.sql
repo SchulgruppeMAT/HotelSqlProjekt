7b511ad59535b2da1909d136e9e52b455faa7cf7
MrSirWankAlot <maincrafthanke@gmail.com>
2018-10-05 10:31:15 +0200

    Überarbeitete SQL Datei nach Norm Unterricht

--- SQL Scripts/create Tables.sql
+++ SQL Scripts/create Tables.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE Hotel(
 HotelNr INT NOT NULL,
-Hotel CHAR
+Hotel CHAR(20),
 PRIMARY KEY (HotelNr)
 )
 go
 CREATE TABLE Art(
 ArtNr INT NOT NULL,
-Art CHAR
+Art CHAR,
 PRIMARY KEY (ArtNr)
 )
 go
@@ -14,39 +14,39 @@ CREATE TABLE Zimmer(
 ZimmerNr INT,
 HotelNr INT,
 KategorieNr INT,
-Zimmer_Beschreibung CHAR
-PRIMARY KEY (ZimmerNr)
-FOREIGN KEY (HotelNr) REFERENCES Hotel
+Zimmer_Beschreibung CHAR,
+PRIMARY KEY (ZimmerNr),
+FOREIGN KEY (HotelNr) REFERENCES Hotel,
 FOREIGN KEY (KategorieNr) REFERENCES Kategorie
 )
 go
 CREATE TABLE Kategorie(
 KategorieNr INT,
 Kategorie CHAR,
-Preis INT
+Preis INT,
 PRIMARY KEY (KategorieNr)
 )
 go
 CREATE TABLE Kunde(
 KundenNr INT,
 Vorname CHAR,
-Nachname CHAR
+Nachname CHAR,
 PRIMARY KEY (KundenNr)
 )
 go
 CREATE TABLE Mitarbeiter(
 MitarbeiterNr INT,
 Vorname CHAR,
-Nachname CHAR
+Nachname CHAR,
 PRIMARY KEY (MitarbeiterNr)
 )
 go
 CREATE TABLE Buchung(
 BuchungNr INT,
 KundenNr INT,
-MitarbeiterNr INT
-PRIMARY KEY (BuchungNr)
-FOREIGN KEY (KundenNr) REFERENCES Kunde
+MitarbeiterNr INT,
+PRIMARY KEY (BuchungNr),
+FOREIGN KEY (KundenNr) REFERENCES Kunde,
 FOREIGN KEY (MitarbeiterNr) REFERENCES Mitarbeiter
 )
 CREATE TABLE Position(
@@ -54,8 +54,8 @@ PositionNr INT,
 BuchungNr INT,
 ZimmerNr INT,
 Anreise INT,
-Abreise INT
-PRIMARY KEY (PositionNr)
-FOREIGN KEY (BuchungNr) REFERENCES Buchung
+Abreise INT,
+PRIMARY KEY (PositionNr),
+FOREIGN KEY (BuchungNr) REFERENCES Buchung,
 FOREIGN KEY (ZimmerNr) REFERENCES Zimmer
 )
