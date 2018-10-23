033732f1f6bb94a080a0659f9af1012602850b54
Demon\ThunFisch <arinartz4@gmail.com>
2018-10-23 10:49:33 +0200

    Mit den Moodle Begonnen

---
+++ script-javascript/App.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    
+    const $body = $('body');
+    
+    // bxslider Config
+    $('.slider').bxSlider({
+        slideWidth: 1600,
+        slideHeight: 200,
+        minSlides: 1,
+        slideMargin: 0,
+        controls: false,
+        auto: true,
+        pause: 3500,
+        pager: false
+    });
+    
+    //offerLayer
+    $body.find()
+
+});
