e077337440025a15ba8ec80005e6c87da4ef87b9
MrSirWankAlot <maincrafthanke@googlemail.com>
2018-10-23 19:12:12 +0200

    Vollendung des sql scripts der anmeldung/registrierung

--- script-sql/login.sql
+++ script-sql/login.sql
@@ -3,5 +3,7 @@ USE Hotel;
 CREATE TABLE LoginData
 (
     username VARCHAR(30) not null,
-    passwort VARCHAR(255) not null
+    passwort VARCHAR(255) not null,
+    fullname  VARCHAR(30) not null,
+    email VARCHAR(255) not null
 );
