3f26051d48bb1e8bc5f8b7af1775d871a4ae7c91
Demon\ThunFisch <arinartz4@gmail.com>
2018-10-24 19:18:27 +0200

    Verbesserter ExitButton

--- script-javascript/App.js
+++ script-javascript/App.js
@@ -66,4 +66,23 @@ $(document).ready(function () {
         });
     })
 
+
+    //text
+    $(function() {
+        //----- OPEN
+        $('[data-popup-open]').on('click', function(e) {
+            var targeted_popup_class = jQuery(this).attr('data-popup-open');
+            $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+    
+            e.preventDefault();
+        });
+    
+        //----- CLOSE
+        $('[data-popup-close]').on('click', function(e) {
+            var targeted_popup_class = jQuery(this).attr('data-popup-close');
+            $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+    
+            e.preventDefault();
+        });
+    });
 });
