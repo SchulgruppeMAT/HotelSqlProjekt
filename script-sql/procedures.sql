7cd0efe104b01db29b239942b93a7247321bdece
DESKTOP-N53T92E\Tobistyle01 <tobistyle27@gmail.com>
2018-11-09 15:17:09 +0100

    Procedures geändert und eine SQL Datei zum erstellen der Datenbank und tabellen für PHPMyAdmin

--- script-sql/procedures.sql
+++ script-sql/procedures.sql
@@ -1,18 +1,18 @@
 --wird noch bearbeitet
-/*
+
 CREATE PROCEDURE
-FreeRooms @firstdate DATE, @seconddate DATE
+FreeRooms @thisday DATE
 AS
-SELECT * FROM Position WHERE ZimmerNr NOT BETWEEN Anreise @firstdate AND @seconddate
-*/
+SELECT Hotel, COUNT(ZimmerNr) AS Freizimmer
+
 --wird noch bearbeitet
 
 GO
 
 CREATE PROCEDURE
-YourBooking @buchungsNr INT
+YourBooking @buchungsNr INT, @kundenNr int
 AS
-SELECT * FROM Position WHERE BuchungsNr = @buchungsNr
+SELECT * FROM Position WHERE BuchungsNr = @buchungsNr AND KundenNr = @KundenNr
 
 GO
 /*
@@ -21,3 +21,21 @@ AllBookings @kundenNr int
 AS
 SELECT  FROM Buchung WHERE 
 */
+
+CREATE PROCEDURE
+AllRooms
+AS
+SELECT Hotel, COUNT(ZimmerNr) AS Zimmeranzahl
+FROM Hotel INNER JOIN Zimmer 
+ON Hotel.HotelNr = Hotel.ZimmerNr
+GROUP BY Hotel
+
+GO
+
+CREATE PROCEDURE
+AllWorker
+AS
+SELECT Hotel, COUNT(MitarbeiterNr) AS Mitarbeiteranzahl
+FROM Hotel INNER JOIN Mitarbeiter
+ON Hotel.HotelNr = Hotel.Mitarbeiter
+GROUP BY Hotel
