1cf51095e0925ebad47c9f56539319dff9a1a028
MrSirWankAlot <maincrafthanke@gmail.com>
2018-11-10 13:44:47 +0100

    Erneiute überarbeitung Bugs behoben wo das Limit für die Werte 1 war.

--- script-sql/PHPMYADMINvers.sql
+++ script-sql/PHPMYADMINvers.sql
@@ -10,58 +10,58 @@ PRIMARY KEY (HotelNr)
 
 CREATE TABLE Art(
 ArtNr INT NOT NULL AUTO_INCREMENT,
-Art CHAR,
+Art CHAR(30),
 PRIMARY KEY (ArtNr)
 );
 
 CREATE TABLE Kategorie(
 KategorieNr INT AUTO_INCREMENT,
-Kategorie CHAR,
-Preis INT,
+Kategorie CHAR(40),
+Preis INT(20),
 PRIMARY KEY (KategorieNr)
 );
 
 CREATE TABLE Kunde(
 KundenNr INT AUTO_INCREMENT,
-Vorname CHAR,
-Nachname CHAR,
+Vorname CHAR(30),
+Nachname CHAR(30),
 PRIMARY KEY (KundenNr)
 );
 
 CREATE TABLE Mitarbeiter(
 MitarbeiterNr INT AUTO_INCREMENT,
-Vorname CHAR,
-Nachname CHAR,
+Vorname CHAR(30),
+Nachname CHAR(30),
 PRIMARY KEY (MitarbeiterNr)
 );
 
 
 CREATE TABLE Zimmer(
-ZimmerNr INT AUTO_INCREMENT,
-HotelNr INT,
-KategorieNr INT,
-Zimmer_Beschreibung CHAR,
+ZimmerNr INT(255) AUTO_INCREMENT,
+HotelNr INT(255),
+KategorieNr INT(255),
+Zimmer_Beschreibung CHAR(255),
 PRIMARY KEY (ZimmerNr),
 FOREIGN KEY (HotelNr) REFERENCES Hotel(HotelNr),
 FOREIGN KEY (KategorieNr) REFERENCES Kategorie(KategorieNr)
 );
 
 CREATE TABLE Buchung(
-BuchungNr INT AUTO_INCREMENT,
-KundenNr INT,
-MitarbeiterNr INT,
+BuchungNr INT(255) AUTO_INCREMENT,
+KundenNr INT(255),
+MitarbeiterNr INT(255),
 PRIMARY KEY (BuchungNr),
 FOREIGN KEY (KundenNr) REFERENCES Kunde(KundenNr),
 FOREIGN KEY (MitarbeiterNr) REFERENCES Mitarbeiter(MitarbeiterNr)
 );
 
-CREATE TABLE Position(
-PositionNr INT AUTO_INCREMENT,
-BuchungNr INT,
-ZimmerNr INT,
+CREATE TABLE Posietion(
+PosietionNr INT(255) AUTO_INCREMENT,
+BuchungNr INT(255),
+ZimmerNr INT(255),
 Anreise DATETIME,
 Abreise DATETIME,
-PRIMARY KEY (PositionNr),
+PRIMARY KEY (PosietionNr),
 FOREIGN KEY (BuchungNr) REFERENCES Buchung(BuchungNr),
 FOREIGN KEY (ZimmerNr) REFERENCES Zimmer(ZimmerNr)
 );
