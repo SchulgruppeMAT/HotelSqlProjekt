2b7e3ace041655c65a4222113c0ea90e80a429f3
MrSirWankAlot <maincrafthanke@gmail.com>
2019-01-21 13:14:59 +0100

    Yolo it may f* everything

--- script-php/login_script.php
+++ script-php/login_script.php
@@ -3,7 +3,7 @@
     include("DBconnection.php");
 
 
-    if(isset($_POST['btc_login'],$_POST['txt_userInput'],$_POST['txt_pw']))
+    if(isset($_POST['btc_login']) && trim($_POST['txt_userInput']) != '' && trim($_POST['txt_pw']) != '')
     {
         // trimming the input
         $inp_username = trim($_POST['txt_userInput']);
@@ -12,6 +12,11 @@
         $stmt = $connection->prepare("SELECT passwort FROM logindata WHERE logindata.username = :txt_user LIMIT 1");
         $stmt->bindParam(':txt_user', $inp_username);
         $stmt->execute();
+
+        // check if username exists
+        if ($stmt->rowCount() == 0){
+          echo "<script>alert('Ungültiger Username');</script>";
+        }
 
         while($row = $stmt->fetch())    // everything gets stored in $row
         {
@@ -38,5 +43,8 @@
                 echo "<script>alert('Falsches PW');</script>";
             }
         }
+    }
+    else if (isset($_POST['btc_login'])){
+      echo "<script>alert('Bitte alle Felder ausfüllen');</script>";
     }
 ?>
