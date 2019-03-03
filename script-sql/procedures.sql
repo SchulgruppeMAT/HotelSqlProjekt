f6635cfe5ce6cbac6c2704a8b4d1b5e312777201
DESKTOP-N53T92E\Tobistyle01 <tobistyle27@gmail.com>
2019-03-03 16:39:50 +0100

    prozeduren für neue Buchungen, kunden, loginData und position hinzugefügt

--- script-sql/procedures.sql
+++ script-sql/procedures.sql
@@ -1,6 +1,3 @@
---fertiges Procedure zum berechnen des Tages ob er frei ist
-USE Hotel;
-
 CREATE DEFINER=`root`@`localhost` 
 PROCEDURE `HowManyFreeRooms`(IN `thisday` DATETIME) 
 NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER
@@ -9,16 +6,15 @@ NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER
  INNER JOIN Zimmer ON Hotel.HotelNr = Zimmer.ZimmerNr 
  INNER JOIN Posietion ON Zimmer.ZimmerNr = Posietion.PosietionNr 
  WHERE thisday NOT BETWEEN Posietion.Anreise AND Posietion.Abreise
- GROUP BY Hotel
+ GROUP BY Hotel;
 
-GO
 
 CREATE DEFINER=`root`@`localhost`
-PROCEDURE `NewBookings`
---wird noch bearbeitet
+PROCEDURE `NewBookings`(IN `kundenNr` INT(255), IN `mitarbeiterNr` INT(255), IN `zimmerNr` INT(255), IN `dateIn` DATETIME, IN `dateOut` DATETIME)
+NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER
+INSERT INTO buchung (KundenNr, MitarbeiterNr) VALUES (kundenNr, mitarbeiterNr);
+INSERT INTO posietion (BuchungNr, ZimmerNr, Anreise, Abreise) VALUES (LAST_INSERT_ID(), zimmerNr, dateIn, dateOut);
 
-GO
--- shows bookings for the sopecific KundenNr
 CREATE DEFINER=`root`@`localhost` 
 PROCEDURE `YourBookings`(IN `kundenNr` INT(255))
  NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER 
@@ -28,51 +24,44 @@ PROCEDURE `YourBookings`(IN `kundenNr` INT(255))
  INNER JOIN Hotel ON zimmer.ZimmerNr = hotel.HotelNr 
  INNER JOIN Buchung ON posietion.BuchungNr = buchung.BuchungNr  
  INNER JOIN kategorie ON zimmer.KategorieNr = kategorie.KategorieNr 
- WHERE Buchung.KundenNr = kundenNr
+ WHERE Buchung.KundenNr = kundenNr;
 
-
-CREATE PROCEDURE
-YourBooking @buchungsNr INT, @kundenNr int
-AS
-SELECT buchung.BuchungNr, buchung.KundenNr, zimmer.ZimmerNr, Hotel.Hotel, posietion.Anreise, posietion.Abreise, kategorie.Kategorie
-  FROM Zimmer
-  INNER JOIN posietion ON Zimmer.ZimmerNr = posietion.ZimmerNr 
-  INNER JOIN Hotel ON zimmer.ZimmerNr = hotel.HotelNr 
-  INNER JOIN Buchung ON posietion.BuchungNr = buchung.BuchungNr  
-  INNER JOIN kategorie ON zimmer.KategorieNr = kategorie.KategorieNr 
-  WHERE Buchung.KundenNr = kundenNr
-
---
--- shows all rooms
 CREATE DEFINER = `root`@`localhost`
 PROCEDURE `AllRooms`()
 NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER
 SELECT COUNT(*) AS Zimmeranzahl
-FROM Hotel INNER JOIN Zimmer ON Hotel.HotelNr = Zimmer.HotelNr
+FROM Hotel INNER JOIN Zimmer ON Hotel.HotelNr = Zimmer.HotelNr;
 
---
-CREATE PROCEDURE
-AllRooms
-AS
-SELECT Hotel, COUNT(ZimmerNr) AS Zimmeranzahl
-FROM Hotel INNER JOIN Zimmer 
-ON Hotel.HotelNr = Hotel.ZimmerNr
-
---
-
--- shows all workers
 CREATE DEFINER=`root`@`localhost`
 PROCEDURE `AllWorker`()
 NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER 
 SELECT mitarbeiter.Vorname, mitarbeiter.Nachname
-FROM Mitarbeiter
+FROM Mitarbeiter;
 
---
 CREATE PROCEDURE
 AllWorker
 AS
 SELECT Hotel, COUNT(MitarbeiterNr) AS Mitarbeiteranzahl
 FROM Hotel INNER JOIN Mitarbeiter
 ON Hotel.HotelNr = Hotel.Mitarbeiter
-GROUP BY Hotel
----
+GROUP BY Hotel;
+
+CREATE DEFINER=`root`@`localhost`
+PROCEDURE `InsertLoginData`(IN `username` VARCHAR(30), IN `passwort` VARCHAR(255), IN `email` VARCHAR(255), IN `vorname` CHAR(30), IN `nachname` CHAR(30), IN `usern` CHAR(30))
+NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER
+INSERT INTO logindata (username, passwort, email) VALUES (username, passwort, email);
+
+CREATE DEFINER=`root`@`localhost`
+PROCEDURE `InsertKundenData`(IN `vorname` CHAR(30), IN `nachname` CHAR(30), IN `usern` CHAR(30))
+NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER
+INSERT INTO kunde (Vorname, Nachname, username) VALUES (vorname, nachname, usern);
+
+CREATE DEFINER=`root`@`localhost` 
+PROCEDURE `InsertPosition`(IN `zimmerNr` INT(255), IN `TagOut` DATETIME, IN `TagIn` DATETIME, IN `buchungNr` INT(255)) 
+NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER 
+INSERT INTO posietion (BuchungNr, ZimmerNr, Anreise, Abreise) VALUES (buchungNr, zimmerNr, TagIn,TagOut);
+
+CREATE DEFINER=`root`@`localhost` 
+PROCEDURE `InsertBuchung`(IN `kundenNr` INT(255), IN `mitarbeiterNr` INT(255)) 
+NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER 
+INSERT INTO Buchung (KundenNr, MitarbeiterNr) VALUES (kundenNr, mitarbeiterNr);
