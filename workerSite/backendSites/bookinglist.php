5fce02b1f4e27f0f4c6f83941295948caa80d379
Demon\ThunFisch <arinartz4@gmail.com>
2019-03-18 19:24:22 +0100

    Userlist ist Done. Tabelle wird ausgegeben und habe bissl CSS dazu gepackt damit es ansehnlicher ist

--- workerSite/backendSites/bookinglist.php
+++ workerSite/backendSites/bookinglist.php
@@ -1,1 +1,42 @@
- 
+<html>
+<style>
+table tr td{
+    border: 1px solid white;
+    padding: 2px;
+    margin: .5%;
+}
+.delete-btn{
+    border: none;
+    border-radius: 10px;
+    background-color: #E23636;
+    color: white;
+    padding: 5px;
+    transition-property: background-color;
+    transition-duration: .5s;
+}
+.delete-btn:hover{
+    background-color: darkred;
+}
+</style>
+<?php
+    include "dbConnect.php";
+
+    $stmt = $conn->prepare("SELECT BuchungsNr, KundenNr, MitarbeiterNr FROM buchung");
+    $stmt->execute();
+?>
+
+    <table>
+<?php
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+    {
+    $buchung = $row['BuchungsNr'];
+    $kundenNr = $row['KundenNr'];
+    $mitarbeiterNr = $row['MitarbeiterNr'];
+
+    echo "<tr>";
+	echo "<td>".$buchung."</td><td>".$kundenNr."</td><td>".$mitarbeiterNr"</td>";
+	echo "</tr>"; 
+    }
+?>
+    </table>
+</html>
--- workerSite/backendSites/userlist.php
+++ workerSite/backendSites/userlist.php
@@ -1,8 +1,41 @@
+<html>
+<style>
+table tr td{
+    border: 1px solid white;
+    padding: 2px;
+    margin: .5%;
+}
+.delete-btn{
+    border: none;
+    border-radius: 10px;
+    background-color: #E23636;
+    color: white;
+    padding: 5px;
+    transition-property: background-color;
+    transition-duration: .5s;
+}
+.delete-btn:hover{
+    background-color: darkred;
+}
+</style>
 <?php
     include "dbConnect.php";
 
-    while($data = $conn->fetch()){
-        echo ($data['username']);
-        echo ($data['email']);
+    $stmt = $conn->prepare("SELECT username, email FROM logindata");
+    $stmt->execute();
+?>
+
+    <table>
+<?php
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+    {
+    $name = $row['username'];
+    $email = $row['email'];
+
+    echo "<tr>";
+	echo "<td>".$name."</td><td>".$email."</td><td><button class='delete-btn'>Delete</button></td>";
+	echo "</tr>"; 
     }
 ?>
+    </table>
+</html>
